a87b648ecb3b296dda927907959e4aee02dedc3b
ricoc236 <ricochen23166@gmail.com>
2026-02-17 22:14:18 -0500

    touching up project description

--- src/components/Projects/Project.vue
+++ src/components/Projects/Project.vue
@@ -14,7 +14,10 @@
 import ProjectCard from './ProjectCard.vue'
 
 const projects = [
-	{ title: 'TCP MultiUser ChatRoom', description: 'Chatroom using Sockets for real-time communication in the terminal.', tags: ['Socket API', 'C++'], url: '#' },
+	{ title: 'TCP MultiUser ChatRoom', description: 'A multithreaded C++ server that manages concurrent client connections via TCP sockets, enabling real-time, terminal-based communication using raw byte streams.', tags: ['Socket API', 'C++', 'POSIX Threads', 'TCP protocol stack', 'Client Server Model'], url: '#' },
+	{ title: 'PlayBoy', description: 'A community-driven platform where users can share Spotify tracks, join real-time music discussions, and build personalized profiles. Using RESTful PHP API and MySQL to manage user generated content, social interactions, and community feeds.', tags: ['React', 'PHP', 'tailwind', 'MySQL', 'Figma', 'scrum', 'agile'], url: '#' },
+	{ title: '8San', description: 'Image sharing Platform that allows user to share and rate images. Developed a secure upload pipeline using FFmpeg to detect and verify file integrity at the binary level and Pillow for automated image manipulation, metadata extraction, and thumbnail generation.', tags: ['Flask', 'CSS', 'JavaScript', 'FFmpeg', 'MongoDB', 'Docker'], url: '#' },
+	{ title: 'AR WhiteBoard', description: 'An augmented reality whiteboard that allows users to draw on a digital canvas with hand gestures.', tags: ['OpenCV', 'MediaPipe', 'NumPy', 'Python', 'Coordinate Mapping'], url: '#' },
 
 ]
 </script>
